e9fe0655659cc1886dd710aab7d1bd610a20aad8
KiSARETO77 <106799027+KiSARETO77@users.noreply.github.com>
2022-10-22 18:58:17 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import aminofix,samino
 PP={"https":"http://dimatjasko10:KWJYs68q@185.112.13.43:2831"}
+comId,chatId=3434136,"59e378d4-1897-4e21-ab42-63861568727f"
 Ç=aminofix.Client(proxies=PP)
 Ç.login("K-HzT@wwjmp.com","GOKU12")
 print("Ready")
@@ -7,9 +8,13 @@ print("Ready")
 def on_text_message(data: aminofix.objects.Event):
 		MSG=data.message.content
 		if ("!REP") in MSG:
-			C=samino.Client(proxies=PP)
-			C.login("zx-@digdig.org","GOKU12")
-			S=samino.Local(comId=3434136,proxies=PP)
-			X=S.get_video_rep_info(chatId="59e378d4-1897-4e21-ab42-63861568727f").json
-			SUB=aminofix.SubClient(comId=3434136,profile=Ç.profile)
-			SUB.send_message(chatId="59e378d4-1897-4e21-ab42-63861568727f",message=f"{X['availableReputation']}")
+			try:
+				C=samino.Client(proxies=PP)
+				C.login("zx-@digdig.org","GOKU12")
+				S=samino.Local(comId=comId,proxies=PP)
+				X=S.get_video_rep_info(chatId=chatId).json
+				SUB=aminofix.SubClient(comId=comId,profile=Ç.profile)
+				SUB.send_message(chatId=chatId,message=f"{X['availableReputation']}")
+			except Exception as F:
+				print(F)
+				pass
