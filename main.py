d5e62c2cfa6e5922fc69ad7467da5036cbda3ba8
KiSARETO77 <106799027+KiSARETO77@users.noreply.github.com>
2022-10-22 19:15:40 +0300

    Update main.py

--- main.py
+++ main.py
@@ -8,13 +8,18 @@ def on_text_message(data: aminofix.objects.Event):
 		MSG=data.message.content
 		comId=data.comId
 		chatId=data.message.chatId
+		MSGID=data.message.messageId
+		userId=data.message.author.userId
 		if ("!REP") in MSG:
-			try:
-				C=samino.Client(proxies=PP)
-				C.login("zx-@digdig.org","GOKU12")
-				S=samino.Local(comId=comId,proxies=PP)
-				X=S.get_video_rep_info(chatId=chatId).json
-				S.send_message(chatId=chatId,message=f"{X['availableReputation']}")
-			except Exception as F:
-				print(F)
-				pass
+			if userId=="6704eb09-1786-4891-b210-c5aafb0c038e"
+				try:
+					C=samino.Client(proxies=PP)
+					C.login("zx-@digdig.org","GOKU12")
+					S=samino.Local(comId=comId,proxies=PP)
+					X=S.get_video_rep_info(chatId=chatId).json
+					C.login("K-HzT@wwjmp.com","GOKU12")
+					S=samino.Local(comId=comId,proxies=PP)
+					S.send_message(chatId=chatId,message=f"Rep : {X['availableReputation']}",replyTo=MSGID)
+				except Exception as F:
+					print(F)
+					pass
